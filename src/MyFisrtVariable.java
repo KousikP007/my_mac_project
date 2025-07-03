47d6a1a07ccebc63a1572901afd2442083c732dd
KousikP007 <paul.kousik.a@gmail.com>
2025-07-03 21:16:54 +0530

    commit for learning variable

---
+++ src/MyFisrtVariable.java
@@ -0,0 +1,7 @@
+public class MyFisrtVariable {
+    public static void main(String[] args) {
+        int a=2;
+        String s="kousik";
+        System.out.println("my first variable"+s);
+    }
+}
