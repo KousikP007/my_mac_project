288a932963d1b61d7d2b09e18f3e311db03ba5f0
KousikP007 <paul.kousik.a@gmail.com>
2025-07-03 21:18:57 +0530

    commit for learning condition

---
+++ src/MyFirstCondition.java
@@ -0,0 +1,8 @@
+public class MyFirstCondition {
+    public static void main(String[] args) {
+        String s="kousik";
+        if(s.equalsIgnoreCase("kouSik")){
+            System.out.println("i learned condition"+s);
+        }
+    }
+}
